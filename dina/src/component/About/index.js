5b82bf19d4d797803cfa3264b332380ea158710d
deenamohdous <dina.mohdous@gmail.com>
2022-06-29 14:40:03 +0300

    start about

--- dina/src/component/About/index.js
+++ dina/src/component/About/index.js
@@ -2,7 +2,16 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About</div>
+    <div className='mainAbout'> 
+         <div className='headerAbout'> 
+      <span>About Me</span>
+    </div>
+    <p className='lineIn'>______________________</p>
+    <div className='pagAbout'>
+
+<p>My Name is Dina I'm a Full-Stack Developer based in Amman . I describe myself as a passionate developer, I enjoy Helping People, and love to learn about new Technologies </p>
+    </div>
+    </div>
   )
 }
 
--- dina/src/component/Menu/index.js
+++ dina/src/component/Menu/index.js
@@ -18,28 +18,47 @@ function Menu() {
         id="offcanvasTop"
         aria-labelledby="offcanvasTopLabel"
       >
+        <div className="closeButton">
+          <button
+            type="button"
+
+            // class="btn-close"
+            // data-bs-dismiss="offcanvas"
+            // aria-label="Close"
+          >
+            <img
+              src="https://img.icons8.com/glyph-neue/64/000000/cancel.png"
+              data-bs-toggle="offcanvas"
+              data-bs-target="#offcanvasTop"
+              aria-controls="offcanvasTop"
+            />
+          </button>
+        </div>
         <div class="offcanvas-header">
           <div class="offcanvas-title" id="offcanvasTopLabel">
-      <Link to={"/home"}>Home</Link>
-      <Link to={"/about"}>About</Link>
-      <Link to={"/project"}>Project</Link>
-      <Link to={"/contact"}>Contact</Link>
-          </div>
-          <button 
-          
-            type="button"
-            class="btn-close"
-            data-bs-dismiss="offcanvas"
-            aria-label="Close"
-          ><img
-          src=""
-          data-bs-toggle="offcanvas"
-          data-bs-target="#offcanvasTop"
-          aria-controls="offcanvasTop"
-        /></button>
+            <div className="socialInfo">
+              <a href="https://www.linkedin.com/in/dina-al-dous">
+                <img src="https://img.icons8.com/ios-glyphs/30/000000/linkedin.png" />
+              </a>
+              <a href="mailto:dina.mohdous@gmail.com">
+                <img src="https://img.icons8.com/material-sharp/24/000000/gmail-new.png" />
+              </a>
+              <a href="https://github.com/Deenamohdous">
+                <img src="https://img.icons8.com/ios-glyphs/30/000000/github.png" />
+              </a>
+              <a href="https://www.instagram.com/">
+                <img src="https://img.icons8.com/ios-glyphs/30/000000/instagram-new.png" />
+              </a>
+            </div>
+            <Link to={"/about"}>About</Link>
+            <Link to={"/home"}>Skills</Link>
+    
+            <Link to={"/project"}>Project</Link>
+            <Link to={"/contact"}>Contact</Link>
+          </div>{" "}
         </div>
-        {/* <div class="offcanvas-body">...</div> */}
       </div>
+      {/* <div class="offcanvas-body">...</div> */}
     </div>
   );
 }
