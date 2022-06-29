b9a329942f417f70febe739357e677771de532f0
deenamohdous <dina.mohdous@gmail.com>
2022-06-29 16:48:23 +0300

    about  pargraph

--- dina/src/component/About/index.js
+++ dina/src/component/About/index.js
@@ -1,15 +1,17 @@
 import React from 'react'
-
+import "./style.css"
 function About() {
   return (
     <div className='mainAbout'> 
          <div className='headerAbout'> 
       <span>About Me</span>
     </div>
-    <p className='lineIn'>______________________</p>
+   <div className='lineAbout'>
+    <p></p>
+   </div>
     <div className='pagAbout'>
 
-<p>My Name is Dina I'm a Full-Stack Developer based in Amman . I describe myself as a passionate developer, I enjoy Helping People, and love to learn about new Technologies </p>
+<p>My Name is <b>Dina</b> I'm a Full-Stack <b>Developer </b> based in Amman . I describe myself as a passionate developer, I enjoy Helping People, and love to learn about new Technologies </p>
     </div>
     </div>
   )
