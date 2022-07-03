446bb6d3125d71100fd80f37e31362f42daa2e27
deenamohdous <dina.mohdous@gmail.com>
2022-07-04 01:39:25 +0300

    css

--- dina/src/component/About/index.js
+++ dina/src/component/About/index.js
@@ -2,13 +2,31 @@ import React from 'react'
 import "./style.css"
 function About() {
   return (
+    
   <div className='main'>
     <div className='mainAbout'> 
-         <div className='headerAbout'> 
+         {/* <div className='headerAbout'> 
          <marquee width="100%" direction="down"  scrollamount="8" height="100px">
          Me, Myself and I.
 </marquee>
-    </div>
+    </div> */}
+    <div class="headerrrrr">
+  <div>M</div>
+  <div>e &nbsp;</div>
+
+  <div> M</div>
+  <div>y</div>
+  <div>s</div>
+  <div>e</div>
+  <div>l</div>
+  <div>f &nbsp;
+</div>
+ 
+  <div> & &nbsp;</div>
+  <div> I</div>
+
+
+</div>
   
     <div className='pagAbout'>
 
--- dina/src/component/Project/index.js
+++ dina/src/component/Project/index.js
@@ -1,9 +1,62 @@
-import React from 'react'
+import React from "react";
+import "./style.css";
 
 function Project() {
   return (
-    <div>Project</div>
-  )
+    <>
+      <div className="dina hederContact"> Projects</div>
+
+      <div className="newDiv">
+        <div class="container">
+          <div class="card">
+            <div class="face face1">
+              <div class="content1">
+                <div class="icon">
+                  <i class="fa fa-linkedin-square" aria-hidden="true"></i>
+                </div>
+              </div>
+            </div>
+            <div class="face face2">
+              <div class="content1">
+                <h3>
+                  <a
+                    href="https://62ad1d7ae39add1060982f66--keen-stroopwafel-441c58.netlify.app/"
+                    target="_blank"
+                  >
+                    Infinity Zone
+                  </a>
+                </h3>
+                <span>
+                  - eCommerce website for electronics products, Where users can
+                  shop online with a real database - Provides all products
+                  according to user interest in categories and brands{" "}
+                </span>
+              </div>
+            </div>
+          </div>
+          <div class="card">
+            <div class="face face1">
+              <div class="content1">
+                <div class="icon">
+                  <i class="fa fa-linkedin-square" aria-hidden="true"></i>
+                </div>
+              </div>
+            </div>
+            <div class="face face2">
+              <div class="content1">
+                <h3>Give Blood</h3>
+                <span>
+                  - Donating blood units, Where users can donate or make an
+                  emergency request - It provides a reliable platform to connect
+                  local blood donors with patients.
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Project
+export default Project;
